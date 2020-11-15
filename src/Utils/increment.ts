59ea05d9ad775ee493e1a0ed87431e2bf20d021d
Hadrien Croubois <hc@iex.ec>
2020-11-15 21:48:28 +0100

    increment/decrement in the 'integers' namespace

--- src/Utils/increment.ts
+++
@@ -1,9 +0,0 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-
-export function incrementBigInt(num: BigInt, amount: BigInt = BigInt.fromI32(1)): BigInt {
-    return num.plus(amount)
-}
-
-export function decrementBigInt(num: BigInt, amount: BigInt = BigInt.fromI32(1)): BigInt {
-    return num.plus(amount)
-}
