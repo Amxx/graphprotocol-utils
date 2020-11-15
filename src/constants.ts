6b7d1f7a74b813af85b90172be45340f11faae71
Hadrien Croubois <hc@iex.ec>
2020-11-15 21:53:32 +0100

    lint

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
-import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 
 export namespace constants {
-	export let   BIGINT_ZERO      = BigInt.fromI32(0);
-	export let   BIGINT_ONE       = BigInt.fromI32(1);
-	export let   BIGDECIMAL_ZERO  = new BigDecimal(constants.BIGINT_ZERO);
-	export const ZERO_ADDRESS     = "0x0000000000000000000000000000000000000000";
-	export const ZERO_BYTES32     = "0x0000000000000000000000000000000000000000000000000000000000000000";
+	export let   BIGINT_ZERO      = BigInt.fromI32(0)
+	export let   BIGINT_ONE       = BigInt.fromI32(1)
+	export let   BIGDECIMAL_ZERO  = new BigDecimal(constants.BIGINT_ZERO)
+	export const ZERO_ADDRESS     = '0x0000000000000000000000000000000000000000'
+	export const ZERO_BYTES32     = '0x0000000000000000000000000000000000000000000000000000000000000000'
 }
