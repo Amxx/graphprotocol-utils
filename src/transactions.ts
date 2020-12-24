2ef47445a7d73965911f74128ff1a03c3fe11bba
Hadrien Croubois <hc@iex.ec>
2020-12-24 12:56:49 +0100

    adding transaction origin

--- src/transactions.ts
+++ src/transactions.ts
@@ -4,13 +4,17 @@ import {
 
 import {
 	Transaction,
+	TransactionSender,
 } from '../generated/schema'
 
 export namespace transactions {
 	export function log(event: ethereum.Event): Transaction {
+		let from = new TransactionSender(event.transaction.from.toHex())
+		from.save()
 		let tx = new Transaction(event.transaction.hash.toHex())
 		tx.timestamp   = event.block.timestamp
 		tx.blockNumber = event.block.number
+		tx.from        = from.id
 		tx.save()
 		return tx as Transaction
 	}
