54ccbd2e1cdeeb89b3e997e9a74148e722f4c13d
Aalavandhan <ottiling@usc.edu>
2021-08-10 16:57:58 -0400

    Added `BIGDECIMAL_ONE` (#7)

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,7 @@ export namespace constants {
 	export let   BIGINT_ZERO      = BigInt.fromI32(0)
 	export let   BIGINT_ONE       = BigInt.fromI32(1)
 	export let   BIGDECIMAL_ZERO  = new BigDecimal(constants.BIGINT_ZERO)
+	export let   BIGDECIMAL_ONE   = new BigDecimal(constants.BIGINT_ONE)
 	export const ADDRESS_ZERO     = '0x0000000000000000000000000000000000000000'
 	export const BYTES32_ZERO     = '0x0000000000000000000000000000000000000000000000000000000000000000'
 }
