b70bd017d2c48884a73dc3898f703cc2ecca7e71
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-09-05 13:25:40 +0200

    Update schema_entry.js

--- scripts/graph-compiler/schema/schema_entry.js
+++ scripts/graph-compiler/schema/schema_entry.js
@@ -42,7 +42,7 @@ class SchemaEntry {
   }
 
   static ensureId(e) {
-    if (e.enums.length == 0 && !e.fields.some(entry => entry.name === 'id')) {
+    if (e.enums.length == 0 && !e.fields.some(field => field.name === 'id')) {
       e.fields.unshift(new SchemaEntryField());
     }
     return e;
